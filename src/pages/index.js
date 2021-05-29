5c93d9c9217faecc8f19667324cfa8bd4bb08fb3
speedsis <speedsis@gmail.com>
2021-05-29 13:55:11 -0300

    Material-UI Configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
         Clone do Youtube com NextJS...
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
     </div>
+    
   )
 }
